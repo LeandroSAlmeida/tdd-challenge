8ad51e1ef1fc8955935d35fc9f133a2c04d46db2
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-04-01 00:11:08 -0300

    feat: implements method delete in CityController and CityServices

--- src/main/java/com/devsuperior/demo/services/CityServices.java
+++ src/main/java/com/devsuperior/demo/services/CityServices.java
@@ -3,9 +3,14 @@ package com.devsuperior.demo.services;
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.services.exceptions.DatabaseException;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,18 +20,31 @@ public class CityServices {
 
     @Autowired
     private CityRepository repository;
-
-
+    @Transactional(readOnly = true)
     public List<CityDTO> findAll(){
         List<City> list = repository.findAll(Sort.by("name"));
         return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional
     public CityDTO insert(CityDTO dto){
         City entity = new City();
         entity.setId(dto.getId());
         entity.setName(dto.getName());
         entity = repository.save(entity);
         return new CityDTO(entity);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
     }
 }
